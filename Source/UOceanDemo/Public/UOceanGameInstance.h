31896309f55f9c57f4de0d4f09cf728d80c55496
skent <skent@DESKTOP-MBVVVUB>
2016-09-07 23:37:50 +0200

    Add project files.

---
+++ Source/UOceanDemo/Public/UOceanGameInstance.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Engine/GameInstance.h"
+#include "UOceanGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UOCEANDEMO_API UUOceanGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
